f5f1ff0e020e072cf3d46fc752500ec359c6ffc2
Zain Ahmad <165908202+ZainSWE@users.noreply.github.com>
2025-03-11 08:17:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const score = {
-        wins: 0,
-        losses: 0,
-        ties: 0,
-        counter: 0
-      };
+      let score = JSON.parse(localStorage.getItem('score'));
+
+      if (!score) {
+        score = {
+          wins: 0,
+          losses: 0,
+          ties: 0
+        }
+      }
 
       function playGame(playerMove) {
         const computerMove = pickComputerMove();
@@ -45,6 +48,8 @@ const score = {
         } else if (result === 'Tie.') {
           score.ties += 1;
         }
+        
+        localStorage.setItem('score', JSON.stringify(score));
 
         alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}\nWins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`);
       }
@@ -70,7 +75,12 @@ const score = {
         score.wins = 0;
         score.losses = 0;
         score.ties = 0;
+        if (!score.counter) {
+          score.counter = 0;
+        }
         score.counter++;
+        localStorage.removeItem('score');
+
         if (score.counter === 1) {
           document.getElementById("reset message").innerHTML = `Score was reset: ${score.counter} time.`;
         } else {
